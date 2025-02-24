d4057f76d1b752cbdbfe7f0298ed275859a2af9c
smanielp <129328326+smanielp@users.noreply.github.com>
2025-02-23 20:17:40 -0700

    Fix next.config.js configuration

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
   basePath: '/golf-app',
-  assetPrefix: '/golf-app/',
 }
 
 module.exports = nextConfig
